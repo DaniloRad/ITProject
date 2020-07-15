e1ec457f5828249b4ee9a5783c623dcdfa3c493d
jovanperunovic <jovanperunovic@github.com>
2020-07-15 20:20:36 +0200

    navbarNaLoginRegister

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,7 +15,128 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 </head>
-<body>
+
+<body class="bg-def ">
+    <div id="modal-user-settings" uk-modal>
+        <div class="uk-modal-dialog">
+            <button class="uk-modal-close-default" type="button" uk-close></button>
+            <div class="uk-modal-header">
+                <h2 class="uk-modal-title">Podesavanja korisnika</h2>
+            </div>
+            <div class="uk-modal-body">
+                <ul class="uk-nav">
+                    <input type="hidden" value="{{Auth::id()}}" id="userId">
+                    <li>
+                        <label for="">Ime:</label>
+                        <input class="uk-input" type="text" id="name">
+                    </li>
+                    <li class="uk-nav-divider"></li>
+                    <li>
+                        <label for="">Email:</label>
+                        <input class="uk-input" type="text" id="email">
+                    </li>
+                    <li class="uk-nav-divider"></li>
+                    <li>
+                        <label for="">Password:</label>
+                        <input class="uk-input" type="text" id="password">
+                    </li>
+                    <li>
+                        <label for="">Slika:</label>
+                        <input type="file" id="image" class="uk-input">
+                    </li>
+                </ul>
+            </div>
+            <div class="uk-modal-footer uk-text-right">
+                <button class="uk-button uk-button-default uk-modal-close" type="button">Otkazi</button>
+                <button class="uk-button uk-button-primary" onclick="updateUser()" type="button">Sacuvaj</button>
+            </div>
+        </div>
+    </div>
+    <div class="header">
+
+        <nav class="uk-navbar-container uk-margin uk-margin-remove-vertical" uk-navbar>
+            <div class="uk-navbar-left">
+
+                <a href="/" class="brand-link">
+                    <img src="{{asset('dist/img/Logo.png')}}"
+                         alt="Logo"
+                         style="opacity: .8; border-radius: 50%;"
+                         width="50" height="50">
+                </a>
+
+                <ul class="uk-navbar-nav">
+                    <li>
+                        <a href="/">
+                           Pocetna
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/recepti">
+                           Recepti
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/emisije">
+                           Emisije
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/pretraga">Pretraga</a>
+                    </li>
+                </ul>
+            </div>
+            
+            <div class="uk-navbar-right">
+
+                <div class="uk-navbar-nav">
+
+                    @if(Auth::check()) 
+                        <li>
+                            <a href="#">{{Auth::user()->name}}</a>
+                        </li>
+
+                        <li class="acc">
+                            <div class="uk-inline ">
+                             <span uk-icon="user"></span>
+                             <div uk-dropdown="mode: click">
+                                 <ul class="uk-nav uk-dropdown-nav">
+                                 
+                                     <li href="#modal-user-settings" uk-toggle><a href="#" onclick="openModal({{Auth::id()}})" class="light-black"><span class="light-black" uk-icon="settings"></span>Podesavanjna korisnika</a></li>
+                                     <li class="uk-nav-divider"></li>
+                                     <li><a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();" class="light-black"> <span class="light-black" uk-icon="sign-out"></span> Odjavi se</a></li>
+                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                         @csrf
+                                     </form> 
+                                 </ul>
+                             </div>
+                         </div>
+                         </li>
+     
+                    @else
+                    <li>
+                        <a href="/login">
+                            Prijava
+                        </a>
+                    </li>
+                    <li>
+                        
+                    </li>  <li>
+                        <a href="/register">
+                           Registracija
+                        </a>
+                    </li>
+                    @endif
+
+                    
+                    
+                </div>
+            </div>
+
+
+        </nav>
+    </div>
+
     <div class="uk-section uk-section-muted uk-flex uk-flex-middle uk-animation-fade" uk-height-viewport>
         <div class="uk-width-1-1">
             <div class="uk-container">
