01ddad10a3b017ba46ce8a6e24b46cd7cbae6a1f
jovanperunovic <jovanperunovic@github.com>
2020-07-16 08:33:32 +0200

    mrsmrsodAdmin

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -7,6 +7,10 @@ use App\Video;
 use App\Category;
 class VideoController extends Controller
 {
+
+    public function __construct() {
+        $this->middleware('admin');
+    }
     /**
      * Display a listing of the resource.
      *
